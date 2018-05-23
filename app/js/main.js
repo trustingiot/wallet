460c820dc959d1bf8629d1a3ac4437fb1af622b2
Fco Javier Estrella Liébana <36156215+javiEstrella@users.noreply.github.com>
2018-05-23 20:45:41 +0200

    minWeightMagnitudeMinimum = 1

--- app/js/main.js
+++ app/js/main.js
@@ -76,7 +76,7 @@ var App = (function(App, undefined) {
   App.uiIsInitialized           = false;
   App.doNodeStarted             = false;
 
-  var minWeightMagnitudeMinimum = (isTestNet ? 9 : 14);
+  var minWeightMagnitudeMinimum = 1;
   var deleteDb                  = false;
   var deleteAnyways             = false;
   var isFullScreen              = false;
